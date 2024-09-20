7d541c56aa1bd16e54ca95b098f5f4b618144139
reslin <uwe.ploss@gmail.com>
2024-09-20 10:59:14 +0200

    count chars

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     file_contents = get_file_contents("books/frankenstein.txt")
-    print(file_contents)
-    print(num_words(file_contents))
+    #print(file_contents)
+    #print(num_words(file_contents))
+    print(num_chars(file_contents))
 
 def get_file_contents(filename):
     with open(filename) as f:
@@ -10,5 +11,15 @@ def get_file_contents(filename):
 def num_words(file_contents):
     words = file_contents.split()
     return len(words)
+
+def num_chars(file_contents):
+    char_count = {}
+    for i in range(len(file_contents)):
+        char = file_contents[i].lower()
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
 
 main()
