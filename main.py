624bb0ad4ce7b236c300cc7661f11017d6af9085
reslin <uwe.ploss@gmail.com>
2024-09-20 10:49:42 +0200

    count words

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    file_contents = get_file_contents("books/frankenstein.txt")
+    print(file_contents)
+    print(num_words(file_contents))
+
+def get_file_contents(filename):
+    with open(filename) as f:
+        return f.read()
+
+def num_words(file_contents):
+    words = file_contents.split()
+    return len(words)
 
 main()
